5d8ac7b9c462f9ea9ee7881e5c9a80a38b02ad03
ofthesparrows <64214227+ofthesparrows@users.noreply.github.com>
2025-04-07 18:00:17 +0100

    Revert "Prep for production"
    This reverts commit 931cc70a54cb2201a8679eb24cb4dc153deb607f.

--- website/settings/base.py
+++ website/settings/base.py
@@ -21,6 +21,9 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+load_dotenv()
+postgres_password = os.getenv('POSTGRES_PASSWORD')
+DEBUG = postgres_password is None
 
 # Application definition
 
--- website/wsgi.py
+++ website/wsgi.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv(os.path.join(PROJECT_DIR, ".env"))
 
 from django.core.wsgi import get_wsgi_application
 
