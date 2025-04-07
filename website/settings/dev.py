d0fdc45f193dc3eccd96ecc63976a8c733afd808
ofthesparrows <64214227+ofthesparrows@users.noreply.github.com>
2025-04-07 18:05:00 +0100

    Update dev.py

--- website/settings/dev.py
+++ website/settings/dev.py
@@ -10,10 +10,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = [
-    'localhost',
-    '206.189.112.151',
-]
+ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
